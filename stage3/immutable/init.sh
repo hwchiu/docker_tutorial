e7d16e873a237dd18ee34ed8951e17f224ea88af
Hung-Wei Chiu <hungwei@opennetworking.org>
2021-12-06 06:32:56 +0000

    Modify example for immutable

--- stage3/immutable/init.sh
+++ stage3/immutable/init.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 
-cleanup() {
-    echo "Cleaning up..."
-    kill "${child_pid}"
-    exit
-}
-
-trap cleanup SIGINT SIGTERM
+trap 'echo signal received!; kill "${child_pid}"; wait "${child_pid}";' SIGINT SIGTERM
 
 pip3 install -r requirements.txt
 python3 -m http.server &
